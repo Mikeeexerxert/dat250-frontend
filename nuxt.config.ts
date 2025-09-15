f995459b200304a703688e8ea22556eaedd864d7
bender <ahmad.jahan2001@gmail.com>
2025-09-15 19:46:13 +0200

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
+    typescript: { typeCheck: true },
+    modules: [
+        '@nuxt/content',
+        '@nuxt/eslint',
+        '@nuxt/image',
+        '@nuxt/scripts',
+        '@nuxt/test-utils',
+        '@nuxt/ui',
+        '@pinia/nuxt',
+        '@nuxtjs/i18n'
+    ],
+    css: [ 
+        '~/assets/css/main.scss'
+    ],
+})
